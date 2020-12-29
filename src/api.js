da1aa046cdfc31324e0bbc200272b46cf84fdcb8
gldi687 <39459584+gldi687@users.noreply.github.com>
2020-12-29 23:21:53 +1100

    api

---
+++ src/api.js
@@ -0,0 +1,33 @@
+const base_url = "https://api.rawg.io/api/";
+
+//getting date 
+const getCurrentMonth = () => {
+    const month = new Date().getMonth() + 1;
+    if (month < 10) {
+        return `0${month}`;
+    } else {
+        return month;
+    }
+};
+const getCurrentDay = () => {
+    const day = new Date().getdate();
+    if (day < 10) {
+        return `0${day}`;
+    } else {
+        return day;
+    }
+};
+
+const currentYear = new Date().getUTCFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+
+export const popularGamesURL = () => {
+    return `${base_url}${popular_games}`;
+};
